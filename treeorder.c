cd88ef8e29c92d90a4a66589bbae2acf578666ce
최재영 <dkemf8595@naver.com>
2023-09-14 10:33:39 +0900

    트리 스택 실습

--- treeorder.c
+++ treeorder.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//링크 트리 노드 구조체 정의
+// 링크 트리 노드 구조체 정의
 struct TreeNode {
     int data;
     struct TreeNode* left;
     struct TreeNode* right;
+};
+
+// 스택 노드 구조체 정의
+struct StackNode {
+    struct TreeNode* node;
+    struct StackNode* next;
 };
 
 // 새로운 노드를 생성하는 함수(링크)
@@ -17,62 +23,120 @@ struct TreeNode* createNode(int data) {
     return newNode;
 }
 
-// 전위 순회 (Preorder Traversal) 함수 (링크)
+// 스택 초기화 함수
+struct StackNode* createStack() {
+    return NULL;
+}
+
+// 스택에 노드 추가 함수
+void push(struct StackNode** stack, struct TreeNode* node) {
+    struct StackNode* newNode = (struct StackNode*)malloc(sizeof(struct StackNode));
+    newNode->node = node;
+    newNode->next = *stack;
+    *stack = newNode;
+}
+
+// 스택에서 노드 꺼내는 함수
+struct TreeNode* pop(struct StackNode** stack) {
+    if (*stack == NULL) {
+        return NULL;
+    }
+    struct StackNode* top = *stack;
+    struct TreeNode* node = top->node;
+    *stack = top->next;
+    free(top);
+    return node;
+}
+
+// 전위 순회 함수
 void preorderTraversal(struct TreeNode* root) {
-    if (root != NULL) {
-        printf("%d ", root->data);
-        preorderTraversal(root->left);
-        preorderTraversal(root->right);
+    if (root == NULL) {
+        return;
     }
+
+    struct StackNode* stack = createStack();
+    struct TreeNode* current = root;
+
+    while (current != NULL || stack != NULL) {
+        while (current != NULL) {
+            // 노드 출력
+            printf(" %d", current->data);
+            // 오른쪽 서브트리를 스택에 추가
+            push(&stack, current->right);
+            // 왼쪽 자식으로 이동
+            current = current->left; 
+        }
+        // 스택에서 노드를 꺼내서 오른쪽 서브트리로 이동
+        current = pop(&stack); 
+    }
+    // 스택 메모리 해제
+    free(stack); 
 }
 
-// 중위 순회 (Inorder Traversal) 함수 (링크)
+// 중위 순회 함수
 void inorderTraversal(struct TreeNode* root) {
-    if (root != NULL) {
-        inorderTraversal(root->left);
-        printf("%d ", root->data);
-        inorderTraversal(root->right);
+    if (root == NULL) {
+        return;
     }
+
+    struct StackNode* stack = createStack();
+    struct TreeNode* current = root;
+
+    while (current != NULL || stack != NULL) {
+        while (current != NULL) {
+            // 현재 노드를 스택에 추가
+            push(&stack, current); 
+            // 왼쪽 자식으로 이동
+            current = current->left; 
+        }
+        // 스택에서 노드를 꺼내서 출력
+        current = pop(&stack); 
+        printf(" %d", current->data);
+        // 오른쪽 자식으로 이동
+        current = current->right; 
+    }
+    //메모리 해제
+    free(stack); 
 }
 
-// 후위 순회 (Postorder Traversal) 함수 (링크)
+// 후위 순회 함수
 void postorderTraversal(struct TreeNode* root) {
-    if (root != NULL) {
-        postorderTraversal(root->left);
-        postorderTraversal(root->right);
-        printf("%d ", root->data);
+    if (root == NULL) {
+        return;
     }
-}
 
-// 배열로 표현한 트리 전위 순회 함수
-void ArraypreorderTraversal(struct TreeNode* arr, int index, int size) {
-    if (index < size && arr[index].data != 0) {
-        printf("%d ", arr[index].data);
-        ArraypreorderTraversal(arr, 2 * index + 1, size); // 왼쪽 자식 노드
-        ArraypreorderTraversal(arr, 2 * index + 2, size); // 오른쪽 자식 노드
-    }
-}
+    struct StackNode* stack1 = createStack();
+    struct StackNode* stack2 = createStack();
+    struct TreeNode* current = root;
 
-// 배열로 표현한 트리 중위 순회 함수
-void ArrayinorderTraversal(struct TreeNode* arr, int index, int size) {
-    if (index < size && arr[index].data != 0) {
-        ArrayinorderTraversal(arr, 2 * index + 1, size); // 왼쪽 자식 노드
-        printf("%d ", arr[index].data);
-        ArrayinorderTraversal(arr, 2 * index + 2, size); // 오른쪽 자식 노드
-    }
-}
+    push(&stack1, current);
 
-// 배열로 표현한 트리 후위 순회 함수
-void ArraypostorderTraversal(struct TreeNode* arr, int index, int size) {
-    if (index < size && arr[index].data != 0) {
-        ArraypostorderTraversal(arr, 2 * index + 1, size); // 왼쪽 자식 노드
-        ArraypostorderTraversal(arr, 2 * index + 2, size); // 오른쪽 자식 노드
-        printf("%d ", arr[index].data);
+    while (stack1 != NULL) {
+        // 스택1에서 노드를 꺼내서 스택2에 추가
+        current = pop(&stack1);
+        push(&stack2, current);
+
+        if (current->left != NULL) {
+            push(&stack1, current->left);
+        }
+        if (current->right != NULL) {
+            push(&stack1, current->right);
+        }
     }
+
+    while (stack2 != NULL) {
+        // 스택2에서 노드를 꺼내서 출력
+        current = pop(&stack2); 
+        printf(" %d", current->data);
+    }
+
+    //메모리 해제
+    free(stack1); 
+    free(stack2); 
 }
 
 int main() {
-    // 링크로 표현한 트리 생성
+    // 트리 생성
     struct TreeNode* root = createNode(1);
     root->left = createNode(2);
     root->right = createNode(7);
@@ -85,41 +149,19 @@ int main() {
     root->right->right->left = createNode(10);
     root->right->right->right = createNode(11);
 
-    printf("<Linked Tree>\n");
-    printf("전위 순회");
+    printf("<Traversal with Stack>\n");
+
+    printf("1.전위 순회:\n");
     preorderTraversal(root);
-    printf("\n\n");
+    printf("\n");
 
-    printf("중위 순회");
+    printf("2.중위 순회:\n");
     inorderTraversal(root);
-    printf("\n\n");
+    printf("\n");
 
-    printf("후위 순회");
+    printf("3.후위 순회:\n");
     postorderTraversal(root);
-    printf("\n\n");
-
-    // 배열로 표현한 트리
-    struct TreeNode arr[] = {
-        {1},
-        {2}, {7},
-        {3}, {6}, {8}, {9},
-        {4}, {5}, {0}, {0}, {0}, {0}, {10}, {11}
-    };
-
-    int size = sizeof(arr) / sizeof(arr[0]);
-
-    printf("<Array Tree>\n");
-    printf("전위 순회");
-    ArraypreorderTraversal(arr, 0, size);
-    printf("\n\n");
-
-    printf("중위 순회");
-    ArrayinorderTraversal(arr, 0, size);
-    printf("\n\n");
-
-    printf("후위 순회");
-    ArraypostorderTraversal(arr, 0, size);
-    printf("\n\n");
+    printf("\n");
 
     return 0;
 }

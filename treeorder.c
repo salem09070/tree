7b1a432b299204f953e57bb80ca12417699e0294
최재영 <dkemf8595@naver.com>
2023-11-07 10:26:24 +0900

    실습

--- treeorder.c
+++ treeorder.c
@@ -1,67 +1,67 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <memory.h>
 
-typedef struct TreeNode {
-	int data;
-	struct TreeNode* left, * right;
-}TreeNode;
+#define TRUE 1
+#define FALSE 0
+#define MAX_VERTICES 100
+#define INF 1000L
 
-void main()
-	{ TreeNode * n1, *n2, *n3 ,*n4, *n5 ,*n6 ,*n7,*n8,*n9,*n10,*n11, *n12, *n13, *n14, *n15;
-		n1 = (TreeNode*)malloc(sizeof(TreeNode));
-		n2 = (TreeNode*)malloc(sizeof(TreeNode));
-		n3 = (TreeNode*)malloc(sizeof(TreeNode));
-		n4 = (TreeNode*)malloc(sizeof(TreeNode));
-		n5 = (TreeNode*)malloc(sizeof(TreeNode));
-		n6 = (TreeNode*)malloc(sizeof(TreeNode));
-		n7 = (TreeNode*)malloc(sizeof(TreeNode));
-		n8 = (TreeNode*)malloc(sizeof(TreeNode));
-		n9 = (TreeNode*)malloc(sizeof(TreeNode));
-		n10 = (TreeNode*)malloc(sizeof(TreeNode));
-		n11 = (TreeNode*)malloc(sizeof(TreeNode));
-		n12 = (TreeNode*)malloc(sizeof(TreeNode));
-		n13 = (TreeNode*)malloc(sizeof(TreeNode));
-		n14= (TreeNode*)malloc(sizeof(TreeNode));
-		n15 = (TreeNode*)malloc(sizeof(TreeNode));
+typedef struct GraphType {
+	int n;	// 정점의 개수
+	int weight[MAX_VERTICES][MAX_VERTICES];
+} GraphType;
 
+int selected[MAX_VERTICES];
+int distance[MAX_VERTICES];
 
-		n1->data = 1;
-		n2->data = 2;
-		n3->data = 7;
-		n4->data = 3;
-		n5->data = 6;
-		n6->data = 8;
-		n7->data = 9;
-		n8->data = 4;
-		n9->data = 5;
-		n10->data = NULL;
-		n11->data = NULL;
-		n12->data = NULL;
-		n13->data = NULL;
-		n14->data = 10;
-		n15->data = 11;
-
-		n1->left = n2;
-		n2->left = n4;
-		n3->left = n6;
-		n4->left = n8;
-		n5->left = n10;
-		n6->left = n12;
-		n7->left = n14;
-
-		n1->right = n3;
-		n2->right = n5;
-		n3->right = n7;
-		n4->right = n9;
-		n5->right = n11;
-		n6->right = n13;
-		n7->right = n15;
+// 최소 dist[v] 값을 갖는 정점을 반환
+int get_min_vertex(int n)
+{
+	int v, i;
+	for (i = 0; i < n; i++)
+		if (!selected[i]) {
+			v = i;
+			break;
+		}
+	for (i = 0; i < n; i++)
+		if (!selected[i] && (distance[i] < distance[v])) v = i;
+	return (v);
 }
-preorder(x) {
-	if (x != NULL) {
-		printf(% d, x);
 
+//
+void prim(GraphType* g, int s)
+{
+	int i, u, v;
+
+	for (u = 0; u < g->n; u++)
+		distance[u] = INF;
+	distance[s] = 0;
+	for (i = 0; i < g->n; i++) {
+		u = get_min_vertex(g->n);
+		selected[u] = TRUE;
+		if (distance[u] == INF) return;
+		printf("정점 %d 추가\n", u+1);
+		for (v = 0; v < g->n; v++)
+			if (g->weight[u][v] != INF)
+				if (!selected[v] && g->weight[u][v] < distance[v])
+					distance[v] = g->weight[u][v];
 	}
 }
 
+int main(void)
+{
+	GraphType g = { 10,
+	{{ 0, 3, INF, INF, INF, 11, 12, INF, INF, INF },
+	{ 3, 0, 5, 4, 1, 7, 8, INF, INF, INF },
+	{ INF, 5, 0, 2, INF, INF, 6, 5, INF, INF },
+	{ INF, 4, 2, 0, 13, INF, INF, 14, INF, 16 },
+	{ INF, 1, INF, 13, 0, 9, INF, INF, 18, 17 },
+	{ 11, 7, INF, INF, 9, 0, INF, INF, INF, INF },
+	{ 12, 8, 6, INF, INF, INF, 0, 13, INF, INF },
+	{ INF, INF, 5, 14, INF, INF, 13, 0, INF, 15 },
+	{ INF, INF, INF, INF, 18, INF, INF, INF, 0, 10 },
+	{ INF, INF, INF, 16, 17, INF, INF, INF, 15, 10, 0 }
+	} };
+	prim(&g, 0);
+	return 0;
+}

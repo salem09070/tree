99cdbe25c59168ea284fc4a7bbdf599f4b347634
최재영 <dkemf8595@naver.com>
2023-11-09 12:34:08 +0900

    과제

--- treeorder.c
+++ treeorder.c
@@ -2,66 +2,124 @@
 #include <stdlib.h>
 #include <memory.h>
 
-typedef struct TreeNode {
-	int data;
-	struct TreeNode* left, * right;
-}TreeNode;
+#define TRUE 1
+#define FALSE 0
+#define MAX_VERTICES 100
+#define INF 1000000
 
-void main()
-	{ TreeNode * n1, *n2, *n3 ,*n4, *n5 ,*n6 ,*n7,*n8,*n9,*n10,*n11, *n12, *n13, *n14, *n15;
-		n1 = (TreeNode*)malloc(sizeof(TreeNode));
-		n2 = (TreeNode*)malloc(sizeof(TreeNode));
-		n3 = (TreeNode*)malloc(sizeof(TreeNode));
-		n4 = (TreeNode*)malloc(sizeof(TreeNode));
-		n5 = (TreeNode*)malloc(sizeof(TreeNode));
-		n6 = (TreeNode*)malloc(sizeof(TreeNode));
-		n7 = (TreeNode*)malloc(sizeof(TreeNode));
-		n8 = (TreeNode*)malloc(sizeof(TreeNode));
-		n9 = (TreeNode*)malloc(sizeof(TreeNode));
-		n10 = (TreeNode*)malloc(sizeof(TreeNode));
-		n11 = (TreeNode*)malloc(sizeof(TreeNode));
-		n12 = (TreeNode*)malloc(sizeof(TreeNode));
-		n13 = (TreeNode*)malloc(sizeof(TreeNode));
-		n14= (TreeNode*)malloc(sizeof(TreeNode));
-		n15 = (TreeNode*)malloc(sizeof(TreeNode));
+typedef struct GraphNode {
+    int vertex;
+    int weight;
+    struct GraphNode* next;
+} GraphNode;
 
+typedef struct GraphType {
+    int n;   // 정점의 개수
+    GraphNode* adjList[MAX_VERTICES];
+} GraphType;
 
-		n1->data = 1;
-		n2->data = 2;
-		n3->data = 7;
-		n4->data = 3;
-		n5->data = 6;
-		n6->data = 8;
-		n7->data = 9;
-		n8->data = 4;
-		n9->data = 5;
-		n10->data = NULL;
-		n11->data = NULL;
-		n12->data = NULL;
-		n13->data = NULL;
-		n14->data = 10;
-		n15->data = 11;
+int distance[MAX_VERTICES]; // 시작 정점으로부터의 최단 경로 거리
+int found[MAX_VERTICES]; // 방문한 정점 표시
 
-		n1->left = n2;
-		n2->left = n4;
-		n3->left = n6;
-		n4->left = n8;
-		n5->left = n10;
-		n6->left = n12;
-		n7->left = n14;
-
-		n1->right = n3;
-		n2->right = n5;
-		n3->right = n7;
-		n4->right = n9;
-		n5->right = n11;
-		n6->right = n13;
-		n7->right = n15;
-}
-preorder(x) {
-	if (x != NULL) {
-		printf(% d, x);
-
-	}
+void init_graph(GraphType* g, int n) {
+    g->n = n;
+    for (int i = 0; i < n; i++) {
+        g->adjList[i] = NULL;
+    }
 }
 
+void insert_edge(GraphType* g, int u, int v, int weight) {
+    GraphNode* node = (GraphNode*)malloc(sizeof(GraphNode));
+    node->vertex = v;
+    node->weight = weight;
+    node->next = g->adjList[u];
+    g->adjList[u] = node;
+}
+
+int choose(int distance[], int n, int found[]) {
+    int min = INF;
+    int minpos = -1;
+    for (int i = 0; i < n; i++) {
+        if (distance[i] < min && !found[i]) {
+            min = distance[i];
+            minpos = i;
+        }
+    }
+    return minpos;
+}
+
+void shortest_path(GraphType* g, int start) {
+    for (int i = 0; i < g->n; i++) {//초기화
+        distance[i] = INF;
+        found[i] = FALSE;
+    }
+
+    distance[start] = 0;
+
+    for (int i = 0; i < g->n - 1; i++) {
+        printf("Distance: \n");
+        for (int j = 0; j < g->n; j++)
+            if (distance[j] == INF)
+                printf(" * ");
+            else
+                printf("%d ", distance[j]);
+        printf("\n");
+
+        printf("Found: ");
+        for (int j = 0; j < g->n; j++)
+            printf("%d ", found[j]);
+        printf("\n");
+
+        int u = choose(distance, g->n, found);
+        found[u] = TRUE;
+
+        for (GraphNode* node = g->adjList[u]; node != NULL; node = node->next) {
+            int v = node->vertex;
+            if (!found[v] && distance[u] + node->weight < distance[v]) {
+                distance[v] = distance[u] + node->weight;
+            }
+        }
+    }
+}
+
+void free_graph(GraphType* g) {
+    for (int i = 0; i < g->n; i++) {
+        GraphNode* node = g->adjList[i];
+        while (node != NULL) {
+            GraphNode* temp = node;
+            node = node->next;
+            free(temp);
+        }
+    }
+}
+
+int main(void) {
+    GraphType g;
+    init_graph(&g, 10); 
+
+    int adj_matrix[10][10] = {
+        {0, 3, INF, INF, INF, 11, 12, INF, INF, INF},
+        {3, 0, 5, 4, 1, 7, 8, INF, INF, INF},
+        {INF, 5, 0, 2, INF, INF, 6, 5, INF, INF},
+        {INF, 4, 2, 0, 13, INF, INF, 14, INF, 16},
+        {INF, 1, INF, 13, 0, 9, INF, INF, 18, 17},
+        {11, 7, INF, INF, 9, 0, INF, INF, INF, INF},
+        {12, 8, 6, INF, INF, INF, 0, 13, INF, INF},
+        {INF, INF, 5, 14, INF, INF, 13, 0, INF, 15},
+        {INF, INF, INF, INF, INF, INF, INF, INF, 0, 10},
+        {INF, INF, INF, INF, 17, INF, INF, 15, 10, 0}
+    };
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            if (adj_matrix[i][j] != INF) {
+                insert_edge(&g, i, j, adj_matrix[i][j]);
+            }
+        }
+    }
+    shortest_path(&g, 0);
+
+    free_graph(&g);
+
+    return 0;
+}

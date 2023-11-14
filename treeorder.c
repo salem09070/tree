e4f4a67018e5be7255343f8557df311e37cfba81
최재영 <dkemf8595@naver.com>
2023-11-14 10:35:00 +0900

    커밋되돌리기

--- treeorder.c
+++ treeorder.c
@@ -1,111 +1,210 @@
+//인접행렬
+// #include <stdio.h>
+//#include <stdlib.h>
+//#include <limits.h>
+//
+//#define TRUE 1
+//#define FALSE 0
+//#define MAX_VERTICES   100   
+//#define INF   1000000   /* 무한대 (연결이 없는 경우) */
+//
+//typedef struct GraphType {
+//    int n;   // 정점의 개수
+//    int weight[MAX_VERTICES][MAX_VERTICES];
+//} GraphType;
+//
+//int distance[MAX_VERTICES];/* 시작정점으로부터의 최단경로 거리 */
+//int found[MAX_VERTICES];      /* 방문한 정점 표시 */
+//
+//
+//int choose(int distance[], int n, int found[])
+//{
+//    int i, min, minpos;
+//    min = INT_MAX;
+//    minpos = -1;
+//    for (i = 0; i < n; i++)
+//        if (distance[i] < min && !found[i]) {
+//            min = distance[i];
+//            minpos = i;
+//        }
+//    return minpos;
+//}
+//
+//void shortest_path(GraphType* g, int start)
+//{
+//    int i, u, w;
+//    for (i = 0; i < g->n; i++) /* 초기화 */
+//    {
+//        distance[i] = g->weight[start][i];
+//        found[i] = FALSE;
+//    }
+//    found[start] = TRUE;    /* 시작 정점 방문 표시 */
+//    distance[start] = 0;
+//    for (i = 0; i < g->n - 1; i++) {
+//        printf("Distance: \n");
+//        for (int j = 0; j < g->n; j++)
+//            if (distance[j] == INF)
+//                printf(" * ");
+//            else
+//                printf("%d ", distance[j]);
+//        printf("\n");
+//
+//        printf("Found: ");
+//        for (int j = 0; j < g->n; j++)
+//            printf("%d ", found[j]);
+//        printf("\n");
+//        u = choose(distance, g->n, found);
+//        found[u] = TRUE;
+//        for (w = 0; w < g->n; w++)
+//            if (!found[w])
+//                if (distance[u] + g->weight[u][w] < distance[w])
+//                    distance[w] = distance[u] + g->weight[u][w];
+//    }
+//}
+//
+//int main(void)
+//{
+//    GraphType g = { 10,{
+//
+// { 0, 3, INF, INF, INF, 11, 12, INF, INF, INF },
+// { 3, 0, 5, 4, 1, 7, 8, INF, INF, INF },
+// { INF, 5, 0, 2, INF, INF, 6, 5, INF, INF },
+// { INF, 4, 2, 0, 13, INF, INF, 14, INF, 16 },
+// { INF, 1, INF, 13, 0, 9, INF, INF, 18, 17 },
+// { 11, 7, INF, INF, 9, 0, INF, INF, INF, INF },
+// { 12, 8, 6, INF, INF, INF, 0, 13, INF, INF },
+// { INF, INF, 5, 14, INF, INF, 13, 0, INF, 15 },
+// { INF, INF, INF, INF, 18, INF, INF, INF, 0, 10 },
+// { INF, INF, INF, 16, 17, INF, INF, INF, 15, 10, 0 }
+//    } };
+//    shortest_path(&g, 0);
+//    return 0;
+//}
+// 
+// 
+//연결 리스트
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
+#include <memory.h>
 
+#define TRUE 1
+#define FALSE 0
 #define MAX_VERTICES 100
-#define INF 1000000  // 무한대 (연결이 없는 경우)
+#define INF 1000000
+
+typedef struct GraphNode {
+    int vertex;
+    int weight;
+    struct GraphNode* next;
+} GraphNode;
 
 typedef struct GraphType {
-    int n;  // 정점의 개수
-    int weight[MAX_VERTICES][MAX_VERTICES];
+    int n;   // 정점의 개수
+    GraphNode* adjList[MAX_VERTICES];
 } GraphType;
 
-int A[MAX_VERTICES][MAX_VERTICES]; // 시작정점으로부터의 최단경로 거리
-int path[MAX_VERTICES][MAX_VERTICES];//최단경로 저장 배열
+int distance[MAX_VERTICES]; // 시작 정점으로부터의 최단 경로 거리
+int found[MAX_VERTICES]; // 방문한 정점 표시
 
-// A 배열을 출력하는 함수
-void printA(GraphType* g) {
-    for (int i = 0; i < g->n; i++) {
-        for (int j = 0; j < g->n; j++) {
-            if (A[i][j] == INF)
-                printf("INF ");
-            else
-                printf("%d ", A[i][j]);
+void init_graph(GraphType* g, int n) {
+    g->n = n;
+    for (int i = 0; i < n; i++) {
+        g->adjList[i] = NULL;
+    }
+}
+
+void insert_edge(GraphType* g, int u, int v, int weight) {
+    GraphNode* node = (GraphNode*)malloc(sizeof(GraphNode));
+    node->vertex = v;
+    node->weight = weight;
+    node->next = g->adjList[u];
+    g->adjList[u] = node;
+}
+
+int choose(int distance[], int n, int found[]) {
+    int min = INF;
+    int minpos = -1;
+    for (int i = 0; i < n; i++) {
+        if (distance[i] < min && !found[i]) {
+            min = distance[i];
+            minpos = i;
         }
+    }
+    return minpos;
+}
+
+void shortest_path(GraphType* g, int start) {
+    for (int i = 0; i < g->n; i++) {//초기화
+        distance[i] = INF;
+        found[i] = FALSE;
+    }
+
+    distance[start] = 0;
+
+    for (int i = 0; i < g->n - 1; i++) {
+        printf("Distance: \n");
+        for (int j = 0; j < g->n; j++)
+            if (distance[j] == INF)
+                printf(" * ");
+            else
+                printf("%d ", distance[j]);
         printf("\n");
-    }
-    printf("\n");
-}
 
-void floyd(GraphType* g) {
-    int i, j, k;
-    for (i = 0; i < g->n; i++)
-        for (j = 0; j < g->n; j++) {
-            A[i][j] = g->weight[i][j];
-            if (i != j && A[i][j] < INF)
-                path[i][j] = i;
-            else
-                path[i][j] = -1;
-        }
+        printf("Found: ");
+        for (int j = 0; j < g->n; j++)
+            printf("%d ", found[j]);
+        printf("\n");
 
-    for (k = 0; k < g->n; k++) {
-        for (i = 0; i < g->n; i++)
-            for (j = 0; j < g->n; j++)
-                if (A[i][k] + A[k][j] < A[i][j]) {
-                    A[i][j] = A[i][k] + A[k][j];
-                    path[i][j] = path[k][j];//최단경로 추가
-                }
-    }
-}
-//Path 출력함수 스텍사용
-void printPath(int u, int v) {
-    if (path[u][v] == -1) {
-        printf("No Path\n"); // u에서 v까지 경로가 없는 경우
-        return;
-    }
-    int stack[MAX_VERTICES], top = -1; // 경로를 저장할 스택과 스택의 최상위 인덱스
-    while (v != u) { // 종료 노드에서 시작 노드로 역으로 추적
-        stack[++top] = v; // 현재 노드를 스택에 저장
-        v = path[u][v]; // 다음 노드로 이동
-    }
-    stack[++top] = u; // 시작 노드를 스택에 추가
-    while (top >= 0)
-        printf("%d ", stack[top--]); // 스택을 역순으로 출력하여 경로 표시
-    printf("\n");
-}
+        int u = choose(distance, g->n, found);
+        found[u] = TRUE;
 
-
-
-int main(void)
-{
-	GraphType g = { 10,{
-
- { 0, 3, INF, INF, INF, 11, 12, INF, INF, INF },
- { 3, 0, 5, 4, 1, 7, 8, INF, INF, INF },
- { INF, 5, 0, 2, INF, INF, 6, 5, INF, INF },
- { INF, 4, 2, 0, 13, INF, INF, 14, INF, 16 },
- { INF, 1, INF, 13, 0, 9, INF, INF, 18, 17 },
- { 11, 7, INF, INF, 9, 0, INF, INF, INF, INF },
- { 12, 8, 6, INF, INF, INF, 0, 13, INF, INF },
- { INF, INF, 5, 14, INF, INF, 13, 0, INF, 15 },
- { INF, INF, INF, INF, 18, INF, INF, INF, 0, 10 },
- { INF, INF, INF, 16, 17, INF, INF, INF, 15, 10, 0 }
-	} };
-
-    floyd(&g);
-
-    int start, end, x;
-    for (x = 0; x < 5; x++) {
-        printf("Start Node: ");
-        scanf("%d", &start);
-        printf("End Node : ");
-        scanf("%d", &end);
-
-        start--;//배열 떄문에 1 감소시키기
-        end--;//이와 같음
-
-
-        if (start < 0 || start >= g.n || end < 0 || end >= g.n) {//노드 초과 또는 0이하 입력시 오류메세지 송출
-            printf("Vertices are out of range\n");
-        }
-        else {
-            printf("Shortest Distance: %d\n", A[start][end]);
-            printf("Path: ");
-            printPath(start, end);
-            printf("\n");
+        for (GraphNode* node = g->adjList[u]; node != NULL; node = node->next) {
+            int v = node->vertex;
+            if (!found[v] && distance[u] + node->weight < distance[v]) {
+                distance[v] = distance[u] + node->weight;
+            }
         }
     }
-    
+}
+
+void free_graph(GraphType* g) {
+    for (int i = 0; i < g->n; i++) {
+        GraphNode* node = g->adjList[i];
+        while (node != NULL) {
+            GraphNode* temp = node;
+            node = node->next;
+            free(temp);
+        }
+    }
+}
+
+int main(void) {
+    GraphType g;
+    init_graph(&g, 10);
+
+    int adj_matrix[10][10] = {
+        {0, 3, INF, INF, INF, 11, 12, INF, INF, INF},
+        {3, 0, 5, 4, 1, 7, 8, INF, INF, INF},
+        {INF, 5, 0, 2, INF, INF, 6, 5, INF, INF},
+        {INF, 4, 2, 0, 13, INF, INF, 14, INF, 16},
+        {INF, 1, INF, 13, 0, 9, INF, INF, 18, 17},
+        {11, 7, INF, INF, 9, 0, INF, INF, INF, INF},
+        {12, 8, 6, INF, INF, INF, 0, 13, INF, INF},
+        {INF, INF, 5, 14, INF, INF, 13, 0, INF, 15},
+        {INF, INF, INF, INF, INF, INF, INF, INF, 0, 10},
+        {INF, INF, INF, INF, 17, INF, INF, 15, 10, 0}
+    };
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            if (adj_matrix[i][j] != INF) {
+                insert_edge(&g, i, j, adj_matrix[i][j]);
+            }
+        }
+    }
+    shortest_path(&g, 0);
+
+    free_graph(&g);
 
     return 0;
 }

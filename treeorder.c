a8931f1a9c49cd2bd300b8af24e4ae66f8c1a99c
salem09070 <144295796+salem09070@users.noreply.github.com>
2023-11-28 10:32:14 +0900

    병합정렬

--- treeorder.c
+++ treeorder.c
@@ -2,110 +2,109 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define SWAP(x, y, t) ( (t)=(x), (x)=(y), (y)=(t) )
 #define MAX_SIZE 20
 
-void selection_sort(int list[], int n, int* compare_count, int* move_count, int iteration)
-{
-    int i, j, least, temp;
-    *compare_count = 0;
-    *move_count = 0;
+int sorted[MAX_SIZE];
+int total_compare = 0, total_move = 0;
 
-    for (i = 0; i < n - 1; i++) {
-        least = i;
-        for (j = i + 1; j < n; j++) {
-            (*compare_count)++;
-            if (list[j] < list[least]) least = j;
+// 전체 배열을 출력하는 함수
+void print_array(int list[], int size) {
+    printf("[");
+    for (int i = 0; i < size; i++) {
+        printf("%d", list[i]);
+        if (i < size - 1) printf(", ");
+    }
+    printf("]\n");
+}
+
+// 두 부분 배열을 합병하는 함수
+void merge(int list[], int left, int mid, int right, int* compare_count, int* move_count, int is_first_iteration) {
+    int i = left, j = mid + 1, k = left;
+    while (i <= mid && j <= right) {
+        (*compare_count)++;
+        if (list[i] <= list[j]) {
+            sorted[k++] = list[i++];
         }
-        SWAP(list[i], list[least], temp);
+        else {
+            sorted[k++] = list[j++];
+        }
         (*move_count)++;
-        if (iteration == 0) { // 첫 번째 정렬 과정 출력
-            for (int k = 0; k < n; k++) {
-                printf("%d ", list[k]);
-            }
-            printf("\n");
-        }
     }
-}
 
-void insertion_sort(int list[], int n, int* compare_count, int* move_count, int iteration) {
-    int i, j, key;
-    *compare_count = 0;
-    *move_count = 0;
-
-    for (i = 1; i < n; i++) {
-        key = list[i];
-        for (j = i - 1; j >= 0 && list[j] > key; j--) {
-            list[j + 1] = list[j];
-            (*compare_count)++;
-            (*move_count)++;
-        }
-        list[j + 1] = key;
+    while (i <= mid) {
+        sorted[k++] = list[i++];
         (*move_count)++;
-        if (iteration == 0) { // 첫 번째 정렬 과정 출력
-            for (int k = 0; k < n; k++) {
-                printf("%d ", list[k]);
-            }
-            printf("\n");
+    }
+    while (j <= right) {
+        sorted[k++] = list[j++];
+        (*move_count)++;
+    }
+
+    for (int l = left; l <= right; l++) {
+        list[l] = sorted[l];
+    }
+
+    // 첫 번째 반복에서만 배열의 상태 출력
+    if (is_first_iteration) {
+        print_array(list, MAX_SIZE);
+    }
+}
+
+// 반복적 병합 정렬 함수
+void iterative_merge_sort(int list[], int size, int* compare_count, int* move_count, int is_first_iteration) {
+    for (int width = 1; width < size; width *= 2) {
+        for (int i = 0; i < size; i = i + 2 * width) {
+            int left = i;
+            int mid = i + width - 1;
+            int right = i + 2 * width - 1;
+            if (right >= size) right = size - 1;
+            if (mid >= size) break; // 배열 크기를 초과하는 너비를 피하기 위함
+            merge(list, left, mid, right, compare_count, move_count, is_first_iteration);
+        }
+        // 각 전체 패스 후, 첫 번째 반복에서 배열을 출력
+        if (is_first_iteration) {
+            print_array(list, size);
         }
     }
 }
 
-void bubble_sort(int list[], int n, int* compare_count, int* move_count, int iteration) {
-    int i, j, temp;
-    *compare_count = 0;
-    *move_count = 0;
-
-    for (i = n - 1; i > 0; i--) {
-        for (j = 0; j < i; j++){ //앞뒤 레코드 비교후 교체
-            (*compare_count)++;
-            if (list[j] > list[j + 1]) {
-                SWAP(list[j], list[j + 1], temp);
-                (*move_count)++;
-            }
-        }
-        if (iteration == 0) { // 첫 번째 정렬 과정 출력
-            for (int k = 0; k < n; k++) {
-                printf("%d ", list[k]);
-            }
-            printf("\n");
-        }
-    }
-}
-
-
-
-int main(void)
-{
-    int list[MAX_SIZE], n, i;
-    int total_compare = 0, total_move = 0;
-    int compare_count, move_count;
-
-    n = MAX_SIZE;
+int main(void) {
+    int list[MAX_SIZE];
     srand(time(NULL));
 
     for (int count = 0; count < 20; count++) {
-        for (i = 0; i < n; i++) {
+        // 각 반복마다 비교 및 이동 횟수를 초기화
+        int compare_count = 0, move_count = 0;
+
+        // 각 반복마다 랜덤 숫자로 리스트 채우기
+        for (int i = 0; i < MAX_SIZE; i++) {
             list[i] = rand() % 100;
         }
 
-
-        if (count == 0) { // 첫 번째 실행에서 난수 배열 및 정렬 과정 출력
-            printf("첫 번째 난수 배열 정렬 과정: 선택 정렬\n");
-            printf("첫 번째 난수 배열 정렬 과정: 삽입 정렬\n");
-            printf("첫 번째 난수 배열 정렬 과정: 버블 정렬\n");
+        // 첫 번째 반복에서만 원본 및 정렬된 배열 출력
+        if (count == 0) {
+            printf("Original list:\n");
+            print_array(list, MAX_SIZE);
+            printf("Merge Sort\n");
         }
-        //selection_sort(list, n, &compare_count, &move_count, count);
-        //insertion_sort(list, n, &compare_count, &move_count, count);
-        bubble_sort(list, n, &compare_count, &move_count, count);
-        total_compare += compare_count;//총 비교 횟수 
-        total_move += move_count;//총 이동 횟수
 
+        // 반복적 병합 정렬 수행
+        iterative_merge_sort(list, MAX_SIZE, &compare_count, &move_count, count == 0);
+
+        // 첫 번째 반복에서만 정렬된 배열 출력
+        if (count == 0) {
+            printf("Sorted list:\n");
+            print_array(list, MAX_SIZE);
+        }
+
+        // 총 비교 및 이동 횟수에 더하기
+        total_compare += compare_count;
+        total_move += move_count;
     }
 
-    printf("평균 이동 횟수: %f\n", (double)total_move / 20);
+    // 평균 비교 및 이동 횟수 계산 및 출력
     printf("평균 비교 횟수: %f\n", (double)total_compare / 20);
-  
+    printf("평균 이동 횟수: %f\n", (double)total_move / 20);
 
     return 0;
 }

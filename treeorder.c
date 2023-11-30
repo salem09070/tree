e8a9f0630ce32a9c3a97a99b15f5a9d5d204a20f
salem09070 <144295796+salem09070@users.noreply.github.com>
2023-11-30 14:02:12 +0900

    퀵솔트 실습

--- treeorder.c
+++ treeorder.c
@@ -1,67 +1,92 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <memory.h>
 
-typedef struct TreeNode {
-	int data;
-	struct TreeNode* left, * right;
-}TreeNode;
+#define MAX_SIZE 20 // 예시 크기
+#define SWAP(x, y, t) ((t)=(x), (x)=(y), (y)=(t))
 
-void main()
-	{ TreeNode * n1, *n2, *n3 ,*n4, *n5 ,*n6 ,*n7,*n8,*n9,*n10,*n11, *n12, *n13, *n14, *n15;
-		n1 = (TreeNode*)malloc(sizeof(TreeNode));
-		n2 = (TreeNode*)malloc(sizeof(TreeNode));
-		n3 = (TreeNode*)malloc(sizeof(TreeNode));
-		n4 = (TreeNode*)malloc(sizeof(TreeNode));
-		n5 = (TreeNode*)malloc(sizeof(TreeNode));
-		n6 = (TreeNode*)malloc(sizeof(TreeNode));
-		n7 = (TreeNode*)malloc(sizeof(TreeNode));
-		n8 = (TreeNode*)malloc(sizeof(TreeNode));
-		n9 = (TreeNode*)malloc(sizeof(TreeNode));
-		n10 = (TreeNode*)malloc(sizeof(TreeNode));
-		n11 = (TreeNode*)malloc(sizeof(TreeNode));
-		n12 = (TreeNode*)malloc(sizeof(TreeNode));
-		n13 = (TreeNode*)malloc(sizeof(TreeNode));
-		n14= (TreeNode*)malloc(sizeof(TreeNode));
-		n15 = (TreeNode*)malloc(sizeof(TreeNode));
+int comparisons = 0; // 비교 횟수
+int swaps = 0; // 스왑 횟수
 
-
-		n1->data = 1;
-		n2->data = 2;
-		n3->data = 7;
-		n4->data = 3;
-		n5->data = 6;
-		n6->data = 8;
-		n7->data = 9;
-		n8->data = 4;
-		n9->data = 5;
-		n10->data = NULL;
-		n11->data = NULL;
-		n12->data = NULL;
-		n13->data = NULL;
-		n14->data = 10;
-		n15->data = 11;
-
-		n1->left = n2;
-		n2->left = n4;
-		n3->left = n6;
-		n4->left = n8;
-		n5->left = n10;
-		n6->left = n12;
-		n7->left = n14;
-
-		n1->right = n3;
-		n2->right = n5;
-		n3->right = n7;
-		n4->right = n9;
-		n5->right = n11;
-		n6->right = n13;
-		n7->right = n15;
-}
-preorder(x) {
-	if (x != NULL) {
-		printf(% d, x);
-
-	}
+void print_array(int list[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", list[i]);
+    }
+    printf("\n");
 }
 
+int partition(int list[], int left, int right, int is_first)
+{
+    int pivot, temp;
+    int low, high;
+
+    low = left;
+    high = right + 1;
+    pivot = list[left];
+    do {
+        do {
+            low++;
+            comparisons++; // 비교 횟수 증가
+        } while (low <= right && list[low] < pivot);
+
+        do {
+            high--;
+            comparisons++; // 비교 횟수 증가
+        } while (high >= left && list[high] > pivot);
+
+        if (low < high) {
+            SWAP(list[low], list[high], temp);
+            swaps++; // 스왑 횟수 증가
+            if (is_first) {
+                print_array(list, MAX_SIZE); // 스왑 후 배열 상태 출력
+            }
+        }
+    } while (low < high);
+
+    SWAP(list[left], list[high], temp);
+    swaps++; // 스왑 횟수 증가
+    if (is_first) {
+        print_array(list, MAX_SIZE); // 스왑 후 배열 상태 출력
+    }
+    return high;
+}
+
+void quick_sort(int list[], int left, int right, int is_first)
+{
+    if (left < right) {
+        int q = partition(list, left, right, is_first);
+        quick_sort(list, left, q - 1, is_first);
+        quick_sort(list, q + 1, right, is_first);
+    }
+}
+
+int main(void)
+{
+    int list[MAX_SIZE];
+    int n, i, repeat;
+    n = MAX_SIZE;
+    srand(time(NULL));
+
+    int total_comparisons = 0;
+    int total_swaps = 0;
+    int repeat_count = 20;
+    printf("quick sort\n");
+    for (repeat = 0; repeat < repeat_count; repeat++) {
+        // 배열 초기화
+        for (i = 0; i < n; i++) {
+            list[i] = rand() % 100; // 0부터 99까지의 난수 생성
+        }
+
+        comparisons = 0;
+        swaps = 0;
+        
+        quick_sort(list, 0, n - 1, repeat == 0); // 퀵정렬 호출, 첫 번째 반복에서만 배열 상태 출력
+
+        total_comparisons += comparisons;
+        total_swaps += swaps;
+    }
+
+    printf("평균 비교 횟수: %f\n", (double)total_comparisons / repeat_count);
+    printf("평균 이동(스왑) 횟수: %f\n", (double)total_swaps / repeat_count);
+
+    return 0;
+}
